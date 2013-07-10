ed6e6a4f65b023705a461590dc942d4376fb67fc
Torjus Eidet <torjue@gmail.com>
2013-07-10 02:06:48 +0200

    refac, enforce code style, new helper methods

--- bookie.js
+++ bookie.js
@@ -1,27 +1,36 @@
+var Subsumer = require('subsumer');
+var ko = require('knockout');
+	
 module.exports = function(){
 
-	var Subsumer = require('subsumer');
-	var ko = require('knockout');
 	var kernel = new Subsumer();
 	var registeredModules = [];
 
-	var registerServices = function(services){
+	var registerService = function(service) {
+		if (!!service.singleton) {
+			kernel.bind(service.name).to(service.service);
+		}
+		else {
+			kernel.bind(service.name).to(service.service).asSingleton();
+		}
+	};
+
+	var registerServices = function(services) {
 		for (var index in services) {
-			if(services.hasOwnProperty(index)){
-				var service = services[index];
-				if(!!service.singleton){
-					kernel.bind(service.name).to(service.service);
-				}
-				else{
-					kernel.bind(service.name).to(service.service).asSingleton();
-				}
+			if (services.hasOwnProperty(index)) {
+				registerService(services[index]);
 			}
 		}
 	};
 
-	var registerModules = function(modules){
+	var registerModule = function(module) {
+		kernel.bind(module.name).to(module.module);
+		registeredModules.push(module);
+	};
+
+	var registerModules = function(modules) {
 		for (var index in modules) {
-			if(modules.hasOwnProperty(index)){
+			if (modules.hasOwnProperty(index)) {
 				var module = modules[index];
 				kernel.bind(module.name).to(module.module);
 			}
@@ -29,14 +38,14 @@ module.exports = function(){
 		registeredModules = registeredModules.concat(modules);
 	};
 
-	var resolveAndBindModules = function(){
+	var resolveAndBindModules = function() {
 		for (var index in registeredModules) {
-			if(registeredModules.hasOwnProperty(index)){
+			if (registeredModules.hasOwnProperty(index)) {
 				var module = registeredModules[index];
 				var viewModelInstance = kernel.resolve(module.name);
 				var templateId = module.template ? module.template : module.name;
 				ko.applyBindings(viewModelInstance, document.getElementById(templateId));
-				if(viewModelInstance.init){
+				if (viewModelInstance.init) {
 					viewModelInstance.init();
 				}
 			}
